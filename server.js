f1ccc8cede234d4abb0ca34701df1f490acc8179
Alex Curtis-Slep <alexcurtisslep@Octavines-MacBook-Air.local>
2022-01-31 14:56:57 -0500

    changed listener in server.js on line 32 and switched from nodemon to node

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express(); 
 var cors = require('cors');
 
-const port = 3001;
+// const port = 3001;
 
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -29,6 +29,6 @@ app.get("/api/whoami", headerParser = (req, res, next) => {
   }
 )
 
-var listener = app.listen(port, function () {
-  console.log(`Your app is listening on port ${port}`);
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
