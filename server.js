a52ae7d6f57874ebaa505e5be9fca109a6ae7308
AlexVCS <alexcurtisslep@gmail.com>
2022-02-01 10:20:38 -0500

    changing port logic in server.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
+const PORT = process.env.PORT || 3000;
 require('dotenv').config();
 var express = require('express');
 var app = express(); 
 var cors = require('cors');
-const port = process.env.PORT || 3000
+
 
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 app.use(express.static('public'));
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get("/api/hello", function (req, res) {
@@ -28,5 +29,6 @@ app.get("/api/whoami", headerParser = (req, res, next) => {
   }
 )
 
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
+app.listen(PORT, function () {
+  console.log(`Your app is listening on port ${PORT}`);
+});
