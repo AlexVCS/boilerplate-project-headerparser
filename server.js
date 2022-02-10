0a1f06114dfcd21805acde360df89adbe8a47b5a
AlexVCS <alexcurtisslep@gmail.com>
2022-02-10 08:29:44 -0500

    working to send the users ip address in the JSON response

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express(); 
 var cors = require('cors');
 
-
+app.enable('trust proxy')
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 app.use(express.static('public'));
@@ -17,17 +17,14 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get("/api/whoami", headerParser = (req, res, next) => {
-  JSON.stringify(req.headers),
-  next()
-  }, function (req, res) {
+
+app.get("/api/whoami", function (req, res) {
     res.json({
-      "ipaddress": req.header('host'),
+      "ipaddress": req.ip,
       "language": req.header('accept-language'),
       "software": req.header('user-agent')
     })
-  }
-)
+  })
 
 app.listen(PORT, function () {
   console.log(`Your app is listening on port ${PORT}`);
